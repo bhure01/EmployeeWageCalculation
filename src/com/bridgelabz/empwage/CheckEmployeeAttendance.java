64af90a046f290048d3db67b28c3c0241279256d
bhure01 <dattatribhure@gmail.com>
2021-12-30 19:21:33 +0530

    Added Employee Attendance check with random

---
+++ src/com/bridgelabz/empwage/CheckEmployeeAttendance.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.empwage;
+
+public class CheckEmployeeAttendance {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Calculation Program");
+
+            int IS_PRESENT = 1;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 2; // 0.0 to 1.0
+
+            if( empCheck == IS_PRESENT ){
+                System.out.println("Employee is Present");
+            } else {
+                System.out.println("Employee is Absent");
+            }
+        }
+    }
